ea9eb2aa07c6e497b6d10c49a8f3d836ef1955ae
Berserker <luanbraga.lb@gmail.com>
2021-01-27 17:52:19 -0300

    Update ProjetofullstackApplication.java
    implements CommandLineRunner

--- projetofullstack/src/main/java/com/luanbraga/projetofullstack/ProjetofullstackApplication.java
+++ projetofullstack/src/main/java/com/luanbraga/projetofullstack/ProjetofullstackApplication.java
@@ -1,13 +1,33 @@
 package com.luanbraga.projetofullstack;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luanbraga.projetofullstack.domain.Categoria;
+import com.luanbraga.projetofullstack.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetofullstackApplication {
+public class ProjetofullstackApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetofullstackApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
 
 }

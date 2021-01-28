19fb5fc2ce489c7f1478a46395378e4059267671
Berserker <luanbraga.lb@gmail.com>
2021-01-27 22:27:39 -0300

    Update Categoria.java

--- projetofullstack/src/main/java/com/luanbraga/projetofullstack/domain/Categoria.java
+++ projetofullstack/src/main/java/com/luanbraga/projetofullstack/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.luanbraga.projetofullstack.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -14,8 +17,10 @@ public class Categoria implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -41,6 +46,14 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override

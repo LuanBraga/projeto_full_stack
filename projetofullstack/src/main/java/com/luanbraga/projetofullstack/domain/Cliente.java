f4e6b72a673394b0c9af2a81e8ed038f7fcbcdf9
Berserker <luanbraga.lb@gmail.com>
2021-02-19 15:06:25 -0300

    Update Cliente.java

--- projetofullstack/src/main/java/com/luanbraga/projetofullstack/domain/Cliente.java
+++ projetofullstack/src/main/java/com/luanbraga/projetofullstack/domain/Cliente.java
@@ -29,7 +29,8 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-		
+	
+	@JsonManagedReference
 	@JsonBackReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
